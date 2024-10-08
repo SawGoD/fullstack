69ed4666101a59e217722eb2e9b70b485afe6b65
SawGoD <flay.my.number.43@mail.ru>
2024-10-08 21:31:53 +0300

    feat: ex.1.7

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -6,6 +6,13 @@ const App = () => {
     const [neutral, setNeutral] = useState(0)
     const [bad, setBad] = useState(0)
 
+    const calcTotal = (props) => props.good + props.neutral + props.bad
+    const calcAvg = (props) => (props.good - props.bad) / (props.good + props.neutral + props.bad)
+    const calcPositive = (props) => (props.good / (props.good + props.neutral + props.bad)) * 100
+
+    // if Nan is returned then use 0
+    const errorGate = (props) => (isNaN(props) ? 0 : props)
+
     const Feedback = (props) => {
         return (
             <div>
@@ -24,6 +31,10 @@ const App = () => {
                 <p>Good {props.good}</p>
                 <p>Neutral... {props.neutral}</p>
                 <p>BAD {props.bad}</p>
+                <br />
+                <p>Total {calcTotal(props)}</p>
+                <p>Avg {errorGate(calcAvg(props))}%</p>
+                <p>Pos {errorGate(calcPositive(props))}%</p>
             </div>
         )
     }
